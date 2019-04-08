d58e9e0b554376346324326a02cbe5deeed4ec86
Matt Han <donghan37@gmail.com>
2019-04-08 17:47:00 +0800

    News go back button

--- app/src/main/java/com/example/testapp/ui/ArticleView.java
+++ app/src/main/java/com/example/testapp/ui/ArticleView.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -16,14 +17,30 @@ import com.example.testapp.R;
 import com.example.testapp.db.dao.ArticleDao_Impl;
 import com.example.testapp.db.entity.Article;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ArticleView extends AppCompatActivity {
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_view);
+
+
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setTitle("News Article");
+
 
         ArticleDao_Impl articleDao = new ArticleDao_Impl();
         try {
