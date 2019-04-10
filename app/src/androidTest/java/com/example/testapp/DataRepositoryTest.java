ac69bad68326274353e4a644f47b0c6ddadc34ef
tehtea <jingwei.chew@gmail.com>
2019-04-10 12:14:10 +0800

    Still testing data repository

--- app/src/androidTest/java/com/example/testapp/DataRepositoryTest.java
+++ app/src/androidTest/java/com/example/testapp/DataRepositoryTest.java
@@ -3,57 +3,74 @@ package com.example.testapp;
 import android.app.Application;
 import android.content.Context;
 
-import com.example.testapp.db.entity.Bookmark;
+import com.example.testapp.db.entity.SchoolEntity;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
+import static junit.framework.TestCase.assertEquals;
+
 @RunWith(AndroidJUnit4.class)
 public class DataRepositoryTest {
+    private DataRepository dbRepo;
 
     @Before
     public void beforeEachTestMethod() {
-//        EmpathyApp app = new EmpathyApp();
         Context appContext = InstrumentationRegistry.getTargetContext().getApplicationContext();
         Application app2 = (Application) appContext;
-        System.err.println(app2);
         EmpathyApp actualApp = (EmpathyApp) app2;
-        DataRepository dbRepo = actualApp.getRepository();
-        List<Bookmark> bookmarks = dbRepo.getBookmarks();
-        System.err.println(bookmarks.get(0).getSchoolName());
-//        System.out.println(appContext.getPackageName());
-//        appContext.getApplicationContext();
-//        System.out.println("testResult: " + (app.getPackageName()));
-//        System.out.println("actualResult: " + (appContext instanceof Application));
-//        app = (EmpathyApp) appContext;
-//        System.out.println(app.getPackageCodePath());
-        System.err.println("Invoked before each test method");
+        dbRepo = actualApp.getRepository();
     }
 
     @Test
-    public void findSchools() {
-//        // Test 1
-//        String pattern = "Tao";
-//        List<String> ccas = new ArrayList<>();
-//        ccas.add("ADVENTURE CLUB");
-//        List<String> courses = new ArrayList<>();
-//        courses.add("SCIENCE");
-//        int schoolLevel = 1;
-//
-//        List<SchoolEntity> output;
-//        List<String> expected = new ArrayList<>();
-//        expected.add("TAO NAN SCHOOL");
-//
-//        DataRepository dataRepository = app.getRepository();
-//        output = dataRepository.findSchools(pattern,ccas,courses,schoolLevel,-1);
-//
-//        assertEquals(expected,output.get(0).getSchoolName());
+    public void findSchoolsTestCase1() {
+        String pattern = "Tao";
+        List<String> ccas = new ArrayList<>();
+        ccas.add("ADVENTURE CLUB");
+        List<String> courses = new ArrayList<>();
+        courses.add("SCIENCE");
+        int schoolLevel = 1;
 
+        List<String> expected = new ArrayList<>();
+        String expectedSchlName1 = "TAO NAN SCHOOL";
+        expected.add(expectedSchlName1);
+
+        List<SchoolEntity> rawSearchResults = dbRepo.findSchools(pattern,ccas,courses,schoolLevel,-1);
+        List<String> searchResultsAsString = new ArrayList<>();
+        for (SchoolEntity sekolah : rawSearchResults)
+        {
+            searchResultsAsString.add(sekolah.schoolName);
+        }
+        assertEquals(expected, searchResultsAsString);
+    }
+
+    @Test
+    public void findSchoolsTestCase2() {
+        String pattern = null;
+        List<String> ccas = new ArrayList<>();
+        ccas.add("ART AND CRAFT CLUB");
+        ccas.add("NCC (LAND)");
+        List<String> courses = new ArrayList<>();
+        courses.add("HIGHER MUSIC");
+        int schoolLevel = 2;
+
+        List<String> expected = new ArrayList<>();
+        String expectedSchlName1 = "Nan Chiau High School".toUpperCase();
+        expected.add(expectedSchlName1);
+
+        List<SchoolEntity> rawSearchResults = dbRepo.findSchools(pattern,ccas,courses,schoolLevel,-1);
+        List<String> searchResultsAsString = new ArrayList<>();
+        for (SchoolEntity sekolah : rawSearchResults)
+        {
+            searchResultsAsString.add(sekolah.schoolName);
+        }
+        assertEquals(expected, searchResultsAsString);
     }
 }
