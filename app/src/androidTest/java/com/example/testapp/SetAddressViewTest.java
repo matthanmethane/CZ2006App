77bb5cfc4c7d36ee1bfff216905740d3ae037495
tehtea <jingwei.chew@gmail.com>
2019-04-10 21:57:17 +0800

    Done with the test for validatePostalCode

--- app/src/androidTest/java/com/example/testapp/SetAddressViewTest.java
+++ app/src/androidTest/java/com/example/testapp/SetAddressViewTest.java
@@ -1,48 +1,52 @@
 package com.example.testapp;
 
 import com.example.testapp.ui.SetAddressView;
-import com.example.testapp.viewmodel.AddressViewModel;
 
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.*;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
 public class SetAddressViewTest {
-    private SetAddressView setAddressView = new SetAddressView();
-    private AddressViewModel model = Mockito.mock(AddressViewModel.class);
+    @Rule
+    public ActivityTestRule<SetAddressView> activityRule =
+            new ActivityTestRule(SetAddressView.class);
 
     @Test
-    public void validatePostalCode1() {
+    public void validatePostalCodeTests1() {
         runTest("12345", 1);
+    }
+
+    @Test
+    public void validatePostalCodeTests2() {
         runTest("1234567", 1);
+    }
+
+    @Test
+    public void validatePostalCodeTests3() {
         runTest("ABCDEF",1);
+    }
+
+    @Test
+    public void validatePostalCodeTests4() {
         runTest("111111",2);
+    }
+
+    @Test
+    public void validatePostalCodeTests5() {
         runTest("416729",3);
     }
 
+
     private void runTest(String input, int expected) {
-        System.out.println("Test: postal code is '" + input + "'");
+        SetAddressView setAddressView = activityRule.getActivity();
         int output = setAddressView.validatePostalCode(input);
-        try {
-            when(model.execute().get()).thenReturn(null);
-            System.out.println(verify(model.execute().get()));
-        } catch (Exception e) {
-            System.out.println("HI");
-        }
-        when(model.getValid()).thenReturn(runMockModel(input));
         assertEquals(expected,output);
-        System.out.println("Test: passed");
     }
 
-    private boolean runMockModel(String postalCode) {
-        AddressViewModel m = new AddressViewModel(postalCode);
-        try {
-            m.execute().get();
-        } catch (Exception e) {
-            System.out.println("exception:" + e);
-        }
-        return m.getValid();
-    }
 }
