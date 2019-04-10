b21d2f2b9f4c6cca026bd1b8a0ce1fc69764f6c8
tehtea <jingwei.chew@gmail.com>
2019-04-10 11:40:35 +0800

    Added a method to get the data repository

--- app/src/androidTest/java/com/example/testapp/DataRepositoryTest.java
+++ app/src/androidTest/java/com/example/testapp/DataRepositoryTest.java
@@ -1,36 +1,59 @@
 package com.example.testapp;
 
-import com.example.testapp.db.entity.SchoolEntity;
+import android.app.Application;
+import android.content.Context;
 
+import com.example.testapp.db.entity.Bookmark;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
-import static junit.framework.TestCase.assertEquals;
+@RunWith(AndroidJUnit4.class)
+public class DataRepositoryTest {
 
-public class DataRepositoryTest extends AppCompatActivity {
+    @Before
+    public void beforeEachTestMethod() {
+//        EmpathyApp app = new EmpathyApp();
+        Context appContext = InstrumentationRegistry.getTargetContext().getApplicationContext();
+        Application app2 = (Application) appContext;
+        System.err.println(app2);
+        EmpathyApp actualApp = (EmpathyApp) app2;
+        DataRepository dbRepo = actualApp.getRepository();
+        List<Bookmark> bookmarks = dbRepo.getBookmarks();
+        System.err.println(bookmarks.get(0).getSchoolName());
+//        System.out.println(appContext.getPackageName());
+//        appContext.getApplicationContext();
+//        System.out.println("testResult: " + (app.getPackageName()));
+//        System.out.println("actualResult: " + (appContext instanceof Application));
+//        app = (EmpathyApp) appContext;
+//        System.out.println(app.getPackageCodePath());
+        System.err.println("Invoked before each test method");
+    }
 
     @Test
     public void findSchools() {
-        // Test 1
-        String pattern = "Tao";
-        List<String> ccas = new ArrayList<>();
-        ccas.add("ADVENTURE CLUB");
-        List<String> courses = new ArrayList<>();
-        courses.add("SCIENCE");
-        int schoolLevel = 1;
-
-        List<SchoolEntity> output;
-        List<String> expected = new ArrayList<>();
-        expected.add("TAO NAN SCHOOL");
-
-        DataRepository dataRepository = ((EmpathyApp) getApplication()).getRepository();
-        output = dataRepository.findSchools(pattern,ccas,courses,schoolLevel,-1);
-
-        assertEquals(expected,output.get(0).getSchoolName());
+//        // Test 1
+//        String pattern = "Tao";
+//        List<String> ccas = new ArrayList<>();
+//        ccas.add("ADVENTURE CLUB");
+//        List<String> courses = new ArrayList<>();
+//        courses.add("SCIENCE");
+//        int schoolLevel = 1;
+//
+//        List<SchoolEntity> output;
+//        List<String> expected = new ArrayList<>();
+//        expected.add("TAO NAN SCHOOL");
+//
+//        DataRepository dataRepository = app.getRepository();
+//        output = dataRepository.findSchools(pattern,ccas,courses,schoolLevel,-1);
+//
+//        assertEquals(expected,output.get(0).getSchoolName());
 
     }
 }
